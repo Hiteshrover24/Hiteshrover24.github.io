846a2d5bf44fcc5b0a5d679eec2aaf5e4e2e1b31
Hiteshrover24 <hiteshchamanthi24@gmail.com>
2026-02-08 01:50:41 +0530

    Add subtle motion effect to card elements

---
+++ script.js
@@ -0,0 +1,13 @@
+// subtle motion effect
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    card.style.transform = `rotateX(${-(y - rect.height/2)/20}deg) rotateY(${(x - rect.width/2)/20}deg)`;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = 'rotateX(0) rotateY(0)';
+  });
+});
